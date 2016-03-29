f2739c6e735b361f11bee71594d108782eb35f86
Jaf Crisologo <jafcrisologo@jafs-macbook-pro.local>
2016-03-29 08:44:51 -0600

    Initial Commit

---
+++ mylittlemonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mylittlemonster
+//
+//  Created by Jaf Crisologo on 2016-03-29.
+//  Copyright © 2016 Modium Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
